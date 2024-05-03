b376b6497c3ede9f49b0d6508839b85cf422ce12
Hemant5harma <hemantsharmahare@gmail.com>
2024-05-03 09:29:18 +0530

    start the backend

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +31,4 @@ urlpatterns = [
     path('contact/', views.contact),
     path('about/', views.about),
     path('product/', views.product),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+CATERORY_CHOICES=[
+    {'Men',"men"},
+    {'Women','women'},
+    {'Kids','kids'},
+    {'Features','featured'},
+    {'Latest','latest'}
+]
+
+
+class Product(models.Model):
+    title=models.CharField(max_length=50)
+    price=models.IntegerField()
+    description=models.TextField()
+    size=models.CharField(max_length=20)
+    cateogry=models.CharField(max_length=20,choices=CATERORY_CHOICES)
+    image=models.ImageField(upload_to='media')
